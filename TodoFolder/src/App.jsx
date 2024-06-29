fc89c0635c29d8d62f9156cf848f702bf11cd1fc
Subhajit Manna <subhajitmanna264@gmail.com>
2024-06-29 20:48:50 +0530

    First Commit

---
+++ TodoFolder/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+import Todo from './components/Todo'
+
+
+function App() {
+  return (
+    <>
+      <div className='min-h-screen bg-zinc-900 grid py-4'>
+        <Todo />
+      </div>
+    </>
+  )
+}
+
+export default App
